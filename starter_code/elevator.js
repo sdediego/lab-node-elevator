c5692a94a77ad9b376fd022efe580e7ae31a520e
sdediego <sergio.dediego@outlook.com>
2017-11-06 16:48:54 +0100

    Iteracion 1

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,31 @@
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0;
+    this.direction = '';
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.intervalId = 0;
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+      this.intervalId = setInterval(this.update, 1000);
+  }
+
+  stop() {
+      clearIterval(this.intervalId);
+  }
+
+  update() {
+      this.log();
+  }
   _passengersEnter() { }
   _passengersLeave() { }
   floorUp() { }
   floorDown() { }
   call() { }
-  log() { }
+  log() {
+      console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+  }
 }
 
 module.exports = Elevator;
