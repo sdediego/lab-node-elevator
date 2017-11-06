878bb9dcf48737b07a540f9dc4270026a570c735
sdediego <sergio.dediego@outlook.com>
2017-11-07 00:24:49 +0100

    Done!

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,13 +3,16 @@ class Elevator {
     this.MAX_FLOOR = 10;
     this.GROUND_FLOOR = 0;
     this.floor = 0;
-    this.direction = '';
+    this.direction = 'up';
+    this.waitingList = [];
+    this.passengers = [];
     this.requests = [];
     this.intervalId = 0;
   }
 
-  start() {
-      this.intervalId = setInterval(this.update, 1000);
+  start(update) {
+      this.intervalId = setInterval(update, 1000);
+      setTimeout(() => this.stop(), 15000);
   }
 
   stop() {
@@ -20,8 +23,27 @@ class Elevator {
       this.log();
   }
 
-  _passengersEnter() { }
-  _passengersLeave() { }
+  _passengersEnter() {
+      for (let i = 0; i < this.waitingList.length; i++) {
+          let passenger = this.waitingList[i];
+          if (this.floor === passenger.originFloor) {
+              this.passengers.push(passenger);
+              this.waitingList.splice(i, 1);
+              this.requests.splice(this.requests.indexOf(passenger.originFloor), 1);
+              console.log(`${passenger.name} has enter the elevator`);
+          }
+      }
+  }
+
+  _passengersLeave() {
+      for (let i = 0; i < this.passengers.length; i++) {
+          let passenger = this.passengers[i];
+          if (this.floor === passenger.destinationFloor) {
+              this.passengers.splice(i, 1);
+              console.log(`${passenger.name} has left the elevator`);
+          }
+      }
+  }
 
   floorUp() {
       this.floor < 10 ? this.floor++ : this.floor;
@@ -34,7 +56,10 @@ class Elevator {
   }
 
   call(person) {
-      this.requests.push(person);
+      this.waitingList.push(person);
+      if (this.requests.indexOf(person.originFloor) === -1) {
+            this.requests.push(person.originFloor);
+      }
   }
 
   log() {
--- starter_code/index.js
+++ starter_code/index.js
@@ -2,19 +2,30 @@ const Elevator = require('./elevator.js');
 const Person = require('./person.js');
 
 const elevator = new Elevator();
+const personJuan = new Person('Juan', 2, 4);
+const personMarta = new Person('Marta', 5, 7);
 
-// Go to the top floor
-let top = false;
-while (!top) {
-    elevator.floorUp();
-    elevator.log();
-    elevator.MAX_FLOOR === elevator.floor ? top = true : top;
-}
 
-// Go to the bottom floor
-let ground = false;
-while (!ground) {
-    elevator.floorDown();
-    elevator.log();
-    elevator.GROUND_FLOOR === elevator.floor ? ground = true : ground;
+elevator.call(personJuan);
+elevator.call(personMarta);
+elevator.start(update, 1000);
+
+function update() {
+    if (elevator.direction === 'up' && elevator.floor < elevator.MAX_FLOOR) {
+        elevator.floorUp();
+        elevator.log();
+        elevator.MAX_FLOOR === elevator.floor ? elevator.direction = 'down' : elevator.direction;
+    } else if (elevator.direction === 'down' && elevator.floor > elevator.GROUND_FLOOR) {
+        elevator.floorDown();
+        elevator.log();
+        elevator.GROUND_FLOOR === elevator.floor ? elevator.direction = 'up' : elevator.direction;
+    }
+
+    for (let i = 0; i < elevator.requests.length; i++) {
+        if (elevator.requests.indexOf(elevator.floor) !== -1) {
+            elevator._passengersEnter();
+        }
+    }
+
+    elevator._passengersLeave();
 }
